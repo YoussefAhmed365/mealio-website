e98583cc1d5a1d39194483fb8fad88e0b331b1ee
YoussefAhmed365 <youssef.ahmed5002@gmail.com>
2025-09-30 11:10:28 +0300

    Designed Contact

--- src/components/shared/Field.jsx
+++ src/components/shared/Field.jsx
@@ -2,14 +2,62 @@ const Field = ({ label, type = "text", name, id, value, required = false, placeh
     const classMap = {
         hidden: "hidden",
         text: "w-full px-3 border border-gray-300 rounded-xl focus:outline-hidden focus:ring-2 focus:ring-amber-500 focus:border-transparent transition",
-        email: "w-full px-3 border border-gray-300 rounded-xl focus:outline-hidden focus:ring-2 focus:ring-amber-500 focus:border-transparent focus:invalid:ring-red-500 invalid:ring-red-500 invalid:text-red-500 transition",
+        email: "w-full px-3 border border-gray-300 rounded-xl focus:outline-hidden focus:ring-2 focus:ring-amber-500 focus:border-transparent focus:invalid:ring-red-500 focus:invalid:ring-red-500 focus:invalid:text-red-500 transition",
         password: "w-full px-3 border border-gray-300 rounded-xl focus:outline-hidden focus:ring-2 focus:ring-amber-500 focus:border-transparent transition",
+        textarea: "w-full px-3 border border-gray-300 rounded-xl focus:outline-hidden focus:ring-2 focus:ring-amber-500 focus:border-transparent transition resize-none",
     }
 
-    if (label) {
-        return (
-            <label htmlFor={id} className="w-full relative block">
-                <span className="text-sm/6 font-bold text-gray-700 absolute bottom-10 left-3">{label}</span>
+    if (type === "textarea") {
+        if (label) {
+            return (
+                <label htmlFor={id} className="w-full relative block">
+                    <span className="text-sm/6 font-bold text-gray-700 absolute top-3 left-3">{label}</span>
+                    <textarea
+                        name={name}
+                        id={id}
+                        value={value}
+                        required={required}
+                        placeholder={placeholder}
+                        className={'pt-10 pb-2 ' + classMap['textarea'] + ' ' + className}
+                        autoComplete={autoComplete}
+                        onChange={onChange}
+                    ></textarea>
+                </label>
+            );
+        } else {
+            return (
+                <textarea
+                    name={name}
+                    id={id}
+                    value={value}
+                    required={required}
+                    placeholder={placeholder}
+                    className={'py-2 ' + classMap['textarea'] + ' ' + className}
+                    autoComplete={autoComplete}
+                    onChange={onChange}
+                ></textarea>
+            );
+        }
+    } else {
+        if (label) {
+            return (
+                <label htmlFor={id} className="w-full relative block">
+                    <span className="text-sm/6 font-bold text-gray-700 absolute top-3 left-3">{label}</span>
+                    <input
+                        type={type}
+                        name={name}
+                        id={id}
+                        value={value}
+                        required={required}
+                        placeholder={placeholder}
+                        className={'pt-10 pb-2 ' + classMap[type] + ' ' + className || classMap['text'] + ' pt-10 ' + className}
+                        autoComplete={autoComplete}
+                        onChange={onChange}
+                    />
+                </label>
+            );
+        } else {
+            return (
                 <input
                     type={type}
                     name={name}
@@ -17,26 +65,12 @@ const Field = ({ label, type = "text", name, id, value, required = false, placeh
                     value={value}
                     required={required}
                     placeholder={placeholder}
-                    className={'pt-10 pb-2 ' + classMap[type] || classMap['text'] + ' pt-10 ' + className}
+                    className={'py-2 ' + classMap[type] + ' ' + className || classMap['text'] + ' py-2 ' + className}
                     autoComplete={autoComplete}
                     onChange={onChange}
                 />
-            </label>
-        );
-    } else {
-        return (
-            <input
-                type={type}
-                name={name}
-                id={id}
-                value={value}
-                required={required}
-                placeholder={placeholder}
-                className={'py-2 ' + classMap[type] || classMap['text'] + ' py-2 ' + className}
-                autoComplete={autoComplete}
-                onChange={onChange}
-            />
-        );
+            );
+        }
     }
 };
 
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../components/shared/MainNavbar'
 import Footer from '../components/shared/MainFooter'
 import Field from '../components/shared/Field'
-import Button from '../components/shared/Button'
+
+// Icons
+import { EnvelopeIcon, PhoneIcon, ArrowRightCircleIcon } from '@heroicons/react/24/solid'
 
 const ContactPage = () => {
     return (
         <>
-            <div className='relative h-96'>
+            <div className='relative h-96 mb-20'>
                 <Navbar />
                 <div className='absolute -z-10 top-0 left-1/2 -translate-x-1/2 w-full h-96 bg-amber-50 flex justify-center items-center'>
                     <h1 className='text-4xl font-bold text-gray-800'>Contact Us</h1>
@@ -20,7 +22,42 @@ const ContactPage = () => {
                     </div>
                 </div>
             </div>
-            <main></main>
+            
+            <main className='grid grid-cols-1 md:grid-cols-2 items-center gap-8 px-8 md:px-40'>
+                <div className="col-span-1">
+                    <h5 className='text-md font-medium text-gray-600 mb-2'>WE'RE HERE FOR YOU</h5>
+                    <h1 className='text-6xl text-gray-900 mb-8'><b>Discuss</b> Your Questions With Us</h1>
+                    <h6 className='text-gray-600 mb-10'>Fill out the form below and we'll get back to you as soon as possible.</h6>
+                    <div className="flex justify-start items-center gap-5 mb-6">
+                        <EnvelopeIcon className='size-8 text-amber-600' />
+                        <div>
+                            <h5 className="text-md text-gray-600">E-mail</h5>
+                            <h5 className="text-md font-medium text-gray-700">info@meal.io</h5>
+                        </div>
+                    </div>
+                    <div className="flex justify-start items-center gap-5">
+                        <PhoneIcon className='size-8 text-amber-600' />
+                        <div>
+                            <h5 className="text-md text-gray-600">Phone</h5>
+                            <h5 className="text-md font-medium text-gray-700">+1 (555) 123-4567</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-1">
+                    <form className='flex flex-col gap-y-5 bg-white shadow-lg rounded-3xl p-8'>
+                        <Field label={"Name"} type="text" name="fullName" placeholder="Your full name" required={true} />
+                        <Field label={"Email"} type="email" name="email" placeholder="Your email address" required={true} />
+                        <Field label={"Subject"} type="text" name="subject" placeholder="Subject" required={true} />
+                        <Field type="textarea" name="message" placeholder="Your message" required={true} className={"h-28"} />
+                        <button
+                            type="submit"
+                            className='max-w-fit pl-0.5 pr-6 py-0.5 flex justify-start items-center gap-2 bg-amber-600 text-white font-semibold rounded-full hover:bg-amber-700 disabled:bg-amber-300 focus:ring-4 focus:ring-amber-300 focus:ring-opacity-75 focus:outline-hidden transition-colors duration-200 cursor-pointer'>
+                                <ArrowRightCircleIcon className='size-12' />Send Message
+                        </button>
+                    </form>
+                </div>
+            </main>
+
             <Footer />
         </>
     );
