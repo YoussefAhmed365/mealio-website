0034ac6fcce8866d3a3a09fc11a70eb5fdfe06fc
YoussefAhmed365 <youssef.ahmed5002@gmail.com>
2025-12-24 11:37:31 +0200

    removed lottie files output while build

--- vite.config.js
+++ vite.config.js
@@ -9,17 +9,8 @@ export default defineConfig({
     tailwindcss()
   ],
   build: {
-    // Optional: Increase the warning limit if you are okay with slightly larger chunks
-    // chunkSizeWarningLimit: 1000,
     rollupOptions: {
-      output: {
-        // 1. Fix for "Some chunks are larger than 500 kB"
-        // This separates the heavy lottie player into its own file
-        manualChunks: {
-          lottie: ['@lottiefiles/react-lottie-player', 'lottie-web'],
-        },
-      },
-      // 2. Fix for "Use of eval" warning
+      // Fix for "Use of eval" warning
       onwarn(warning, warn) {
         if (warning.code === 'EVAL' && /lottie/i.test(warning.id)) {
           return;
